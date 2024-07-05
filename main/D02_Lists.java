34a63ba675fc5d539bbc7271657048b321426ad8
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-07-05 13:13:30 +0200

    Add Demo 2 (Lists)

---
+++ main/D02_Lists.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Listen
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class D02_Lists {
+
+  public static void main(String[] args) {
+
+    List<String> names = new LinkedList<>();
+
+  }
+
+}
