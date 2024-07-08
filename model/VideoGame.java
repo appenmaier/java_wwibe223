38b49ecaff72d5372c5fb37d2f1abe04b24b91e2
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-07-08 17:11:56 +0200

    Update VideoGame (Class To Record)

--- model/VideoGame.java
+++ model/VideoGame.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Objects;
-
 /**
  * Videospiel
  *
@@ -9,66 +7,8 @@ import java.util.Objects;
  * @version 1.0
  *
  */
-public class VideoGame implements Comparable<VideoGame> {
-
-  private final String title;
-  private final String publishingYear;
-  private final VideoGameSystem videoGameSystem;
-  private double averageRating;
-
-  public VideoGame(String title, String publishingYear, VideoGameSystem videoGameSystem,
-      double averageRating) {
-    this.title = title;
-    this.publishingYear = publishingYear;
-    this.videoGameSystem = videoGameSystem;
-    this.averageRating = averageRating;
-  }
-
-  public double getAverageRating() {
-    return averageRating;
-  }
-
-  public void setAverageRating(double averageRating) {
-    this.averageRating = averageRating;
-  }
-
-  public String title() {
-    return title;
-  }
-
-  public String publishingYear() {
-    return publishingYear;
-  }
-
-  public VideoGameSystem videoGameSystem() {
-    return videoGameSystem;
-  }
-
-  @Override
-  public String toString() {
-    return "VideoGame [title=" + title + ", publishingYear=" + publishingYear + ", videoGameSystem="
-        + videoGameSystem + ", averageRating=" + averageRating + "]";
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(averageRating, publishingYear, title, videoGameSystem);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    VideoGame other = (VideoGame) obj;
-    return Double.doubleToLongBits(averageRating) == Double.doubleToLongBits(other.averageRating)
-        && Objects.equals(publishingYear, other.publishingYear)
-        && Objects.equals(title, other.title)
-        && Objects.equals(videoGameSystem, other.videoGameSystem);
-  }
+public record VideoGame(String title, String publishingYear, VideoGameSystem videoGameSystem,
+    double averageRating) implements Comparable<VideoGame> {
 
   @Override
   public int compareTo(VideoGame otherVideoGame) {
