7d46489b2a8728e87e02cf9d4a8744cd6fac1ef5
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-08-09 12:01:12 +0200

    Update Demo 11

--- main/D11_JavaStreamAPI.java
+++ main/D11_JavaStreamAPI.java
@@ -1,8 +1,10 @@
 package main;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import model.VideoGame;
 import model.VideoGameSystem;
 
@@ -26,7 +28,7 @@ public class D11_JavaStreamAPI {
     List<String> names = games.stream()
         .filter(g -> g.videoGameSystem()
             .equals(VideoGameSystem.PS4))
-        .map(v -> v.title()) // .map(VideoGame::title)
+        .map(g -> g.title()) // .map(VideoGame::title)
         .toList();
     names.forEach(System.out::println);
     System.out.println();
@@ -53,6 +55,40 @@ public class D11_JavaStreamAPI {
         .max((g1, g2) -> Double.compare(g1.averageRating(), g2.averageRating()));
     bestGame.ifPresent(System.out::println);
     System.out.println();
+
+    // filter, findAny/findFirst
+    System.out.println("Ein Switch-Spiel");
+    Optional<VideoGame> switchGame = games.stream()
+        .filter(g -> g.videoGameSystem()
+            .equals(VideoGameSystem.NSW))
+        .findAny();
+    switchGame.ifPresent(System.out::println);
+    System.out.println();
+
+    // anyMatch/allMatch/noneMatch
+    System.out.println("Gibt es ein PS4-Spiel mit einer Durchschnittsbewertung < 7?");
+    boolean exists = games.stream()
+        // .anyMatch(g -> g.averageRating() < 7 && g.videoGameSystem().equals(VideoGameSystem.PS4));
+        .filter(g -> g.averageRating() < 7)
+        .anyMatch(g -> g.videoGameSystem()
+            .equals(VideoGameSystem.PS4));
+    System.out.println(exists);
+    System.out.println();
+
+    // map, distinct, toList
+    System.out.println("Alle Konsolen als Liste");
+    List<VideoGameSystem> videoGameSystems = games.stream()
+        .map(g -> g.videoGameSystem())
+        .distinct()
+        .toList();
+    videoGameSystems.forEach(System.out::println);
+    System.out.println();
+
+    // collect
+    System.out.println("Alle Spiele nach Konsole gruppiert");
+    Map<VideoGameSystem, List<VideoGame>> gamesByVideoGameSystem = games.stream()
+        .collect(Collectors.groupingBy(g -> g.videoGameSystem()));
+    gamesByVideoGameSystem.forEach((s, gs) -> System.out.println(s + ": " + gs));
 
   }
 
